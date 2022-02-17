bc2437cc69cb6cc53f090447aff33718bf971dd0
Andrés Talavera <andres.talavera@live.fr>
2022-02-17 12:38:06 +0100

    Start element must be AppShell

--- ExpertsBlog.Mobile/App.xaml.cs
+++ ExpertsBlog.Mobile/App.xaml.cs
@@ -10,7 +10,7 @@ namespace ExpertsBlog.Mobile
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new AppShell();
         }
 
         protected override void OnStart()
