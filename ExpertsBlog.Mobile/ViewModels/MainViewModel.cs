0724227da5432e624067a4b44bccbd376c5c2f66
Andrés Talavera <andres.talavera@live.fr>
2022-02-17 09:31:09 +0100

    Add mobile apps

---
+++ ExpertsBlog.Mobile/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using ExpertsBlog.Entities;
+using System.Collections.ObjectModel;
+
+namespace ExpertsBlog.Mobile.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private ObservableCollection<BlogPost> blogPosts = new ObservableCollection<BlogPost>();
+        public ObservableCollection<BlogPost> BlogPosts
+        {
+            get => blogPosts;
+            set => SetProperty(ref blogPosts, value);
+        }
+
+        public MainViewModel()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                BlogPosts.Add(new BlogPost
+                {
+                    Title = "Title " + i
+                });
+            }
+        }
+    }
+}
---
+++ ExpertsBlog.Mobile/ViewModels/ViewModelBase.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ExpertsBlog.Mobile.ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Méthode qui va effectuer la notification
+        /// </summary>
+        private void OnPropertyChanged(string propertyName)
+        {
+            // On vérifie que PropertyChanged n'est pas `null`
+            if (PropertyChanged == null)
+            {
+                return;
+            }
+            // this = instance de `DemoViewModel`
+            // Instancier l'événement qui va contenir le nom de la propriété à changer.
+            // On créé les arguments de l'événement.
+            var args = new PropertyChangedEventArgs(propertyName);
+            // On invoque l'événément en lui donnant le nom de la propriété à modifier.
+            PropertyChanged.Invoke(this, args);
+        }
+
+        /// <summary>
+        /// Méthode générique qui va notifier le changement de valeur d'une propriété.
+        /// </summary>
+        /// <typeparam name="T">Type de la propriété publique et du champ privé.</typeparam>
+        /// <param name="storage">Valeur avec sa réference (adresse en mémoire) du champ privé.</param>
+        /// <param name="value">Nouvelle valeur.</param>
+        /// <param name="propertyName">Le nom de la propriété qui va être modifiée.</param>
+        protected void SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            // Vérifier l'égalité entre la valeur de `storage` et de `value`.
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+            {
+                // Si la valeur est égale, on arrête.
+                return;
+            }
+            // On remplace la valeur à l'adresse de `storage` par la nouvelle valeur, `value`.
+            storage = value;
+            // On appelle la méthode `OnPropertyChanged(string)` qui va créer un événement de changement de valeur.
+            OnPropertyChanged(propertyName);
+        }
+    }
+}
