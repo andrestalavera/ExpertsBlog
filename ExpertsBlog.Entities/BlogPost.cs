cb6fe1936fdeadf2ef136e46a0da2c315d682ef9
Andrés Talavera <andres.talavera@live.fr>
2022-02-17 16:12:56 +0100

    Add Author and ImageUrl properties

--- ExpertsBlog.Entities/BlogPost.cs
+++ ExpertsBlog.Entities/BlogPost.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections.Generic;
 
 namespace ExpertsBlog.Entities
 {
     public class BlogPost : EntityBase
     {
+        public string ImageUrl { get; set; }
+        public string Author { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
         public HashSet<Tag> Tags { get; } = new HashSet<Tag>();
