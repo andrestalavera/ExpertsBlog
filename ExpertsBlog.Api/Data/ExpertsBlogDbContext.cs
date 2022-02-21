f3239ecdb92557e15dac8d6b7fec383f115b8928
Andrés Talavera <andres.talavera@live.fr>
2022-02-21 18:15:50 +0100

    API

--- ExpertsBlog.Api/Data/ExpertsBlogDbContext.cs
+++ ExpertsBlog.Api/Data/ExpertsBlogDbContext.cs
@@ -3,13 +3,19 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ExpertsBlog.Data
 {
+    // La classe ExpertsBlogDbContext hérite de Microsoft.EntityFrameworkCore.DbContext
     public class ExpertsBlogDbContext : DbContext
     {
+        // 4 auto-propriétés de type DbSet<BlogPost>, DbSet<Category>, DbSet<Address> et DbSet<Tag>
+        public DbSet<Address> Addresses { get; set; }
         public DbSet<BlogPost> BlogPosts { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Tag> Tags { get; set; }
 
-        public ExpertsBlogDbContext(DbContextOptions<ExpertsBlogDbContext> options) : base(options)
+        // Constructeur prenant un DbContextOptions<ExpertsBlogDbContext> en paramètre.
+        // Ce constructeur appelle le constructeur de base (DbContext(options))
+        public ExpertsBlogDbContext(DbContextOptions<ExpertsBlogDbContext> options) 
+            : base(options)
         {
         }
     }
--- ExpertsBlog.Api/Program.cs
+++ ExpertsBlog.Api/Program.cs
@@ -8,10 +8,16 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using System;
 
+// Créer un BUILDER pour configurer une application Web
 var builder = WebApplication.CreateBuilder(args);
 
+// On récupère la chaîne de connexion de la base de données
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// On ajoute ExpertsBlogDbContext dans les services. Par défaut, la durée de vie du DbContext est "Scoped"
+// On injecte la configuration la configuration.
 builder.Services.AddDbContext<ExpertsBlogDbContext>(options => options.UseSqlServer(connectionString));
+
+// On ajoute Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
@@ -28,13 +34,18 @@ builder.Services.AddSwaggerGen(options =>
     });
 });
 
+// On construit l'application Web
 var app = builder.Build();
+
+// On utilise Swagger (configuré plus haut)
 app.UseSwagger();
+// On utilise SwaggerUI et on configure l'emplacement du fichier de définition et de l'UI
 app.UseSwaggerUI(options =>
 {
     options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
     options.RoutePrefix = string.Empty;
 });
+// On utilise la redirection de HTTP vers HTTPS
 app.UseHttpsRedirection();
 
 ConfigureRoutesFor<BlogPost>(app, "BlogPosts");
@@ -42,15 +53,21 @@ ConfigureRoutesFor<Address>(app, "Addresses");
 ConfigureRoutesFor<Category>(app, "Categories");
 ConfigureRoutesFor<Tag>(app, "Tags");
 
+// On exécute l'application web
 app.Run();
 
+// Configurer les routes pour (équivalent .NET d'une méthode privée dans la classe Program)
+// Exemples pour BlogPost, applicable pour BlogPost, Address, Category, Tag
 void ConfigureRoutesFor<TEntity>(WebApplication app, string routeBase)
     where TEntity : EntityBase
 {
+    // HTTP GET https://expertsblogapi.azurewebsites.net/BlogPosts
     app.MapGet(routeBase, async (ExpertsBlogDbContext context) => await context.Set<TEntity>().AsNoTracking().ToListAsync());
+
+    // HTTP GET https://expertsblogapi.azurewebsites.net/BlogPosts/1
     app.MapGet($"{routeBase}/{{id}}", async (ExpertsBlogDbContext context, int id) => await context.Set<TEntity>().AsNoTracking().SingleOrDefaultAsync(b => b.Id == id));
     
-
+    // HTTP POST https://expertsblogapi.azurewebsites.net/BlogPosts
     app.MapPost(routeBase, async (ExpertsBlogDbContext context, [FromBody]TEntity entity) =>
     {
         if (entity is not null)
@@ -63,6 +80,8 @@ void ConfigureRoutesFor<TEntity>(WebApplication app, string routeBase)
         }
         throw new ArgumentNullException(nameof(entity));
     });
+    
+    // HTTP PUT https://expertsblogapi.azurewebsites.net/BlogPosts/1
     app.MapPut($"{routeBase}/{{id}}", async (ExpertsBlogDbContext context, int id, [FromBody]TEntity entity) =>
     {
         if (entity is not null)
@@ -75,6 +94,8 @@ void ConfigureRoutesFor<TEntity>(WebApplication app, string routeBase)
         }
         throw new ArgumentNullException(nameof(entity));
     });
+
+    // HTTP DELETE https://expertsblogapi.azurewebsites.net/BlogPosts/1
     app.MapDelete($"{routeBase}/{{id?}}", async (ExpertsBlogDbContext context, int? id) =>
     {
         if (id.HasValue)
