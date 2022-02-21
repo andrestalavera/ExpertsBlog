066372e61736f042c126bcc155ebc9dce0fa8618
Andrés Talavera <andres.talavera@live.fr>
2022-02-21 09:10:03 +0100

    Add DetailsPage and ViewModel on Mobile application

---
+++ ExpertsBlog.Mobile/ViewModels/DetailsViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ExpertsBlog.Mobile.ViewModels
+{
+    [QueryProperty(nameof(Id), nameof(Id))]
+    public class DetailsViewModel : ViewModelBase
+    {
+        private int id;
+        public int Id
+        {
+            get => id;
+            set
+            {
+                SetProperty(ref id, value);
+                LoadItem(value);
+            }
+        }
+
+        private string content;
+        public string Content
+        {
+            get => content;
+            set => SetProperty(ref content, value);
+        }
+
+        private DateTime creation;
+        public DateTime Creation
+        {
+            get => creation;
+            set => SetProperty(ref creation, value);
+        }
+
+        private void LoadItem(int id)
+        {
+            Id = id;
+            Content = "New content";
+            Creation = DateTime.Today;
+        }
+    }
+}
