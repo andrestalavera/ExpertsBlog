81b73e050ed35b4742da48736c1e4eaae04eb89e
Andrés Talavera <andres.talavera@live.fr>
2022-02-23 10:59:19 +0100

    Add GetBlogPost(int) in ApiService

--- ExpertsBlog.Mobile/Services/IExpertsBlogApiService.cs
+++ ExpertsBlog.Mobile/Services/IExpertsBlogApiService.cs
@@ -11,6 +11,7 @@ namespace ExpertsBlog.Mobile.Services
     public interface IExpertsBlogApiService
     {
         Task<IEnumerable<BlogPost>> GetBlogPosts();
+        Task<BlogPost> GetBlogPost(int id);
     }
 
     public class ExpertsBlogApiService : IExpertsBlogApiService
@@ -25,6 +26,24 @@ namespace ExpertsBlog.Mobile.Services
                 var json = await httpclient.GetStringAsync("BlogPosts");
                 Debug.WriteLine("************************************************" + json);
                 return JsonConvert.DeserializeObject<IEnumerable<BlogPost>>(json);
+            }
+        }
+
+        public async Task<BlogPost> GetBlogPost(int id)
+        {
+            using (HttpClient httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://expertsblogapi.azurewebsites.net")
+            })
+            {
+                string blogPostJson = await httpClient.GetStringAsync($"BlogPosts/{id}");
+                BlogPost blogPost = JsonConvert.DeserializeObject<BlogPost>(blogPostJson);
+
+                string categoryJson = await httpClient.GetStringAsync($"Categories/{blogPost.CategoryId}");
+                Category category = JsonConvert.DeserializeObject<Category>(categoryJson);
+
+                blogPost.Category = category;
+                return blogPost;
             }
         }
     }
