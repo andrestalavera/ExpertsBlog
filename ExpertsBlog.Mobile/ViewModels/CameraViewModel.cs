2680b8c6f4a04a8fad7320945dcfd553084f5c26
Andrés Talavera <andres.talavera@live.fr>
2022-02-24 12:17:52 +0100

    Add Camera

---
+++ ExpertsBlog.Mobile/ViewModels/CameraViewModel.cs
@@ -0,0 +1,20 @@
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace ExpertsBlog.Mobile.ViewModels
+{
+    public class CameraViewModel : ViewModelBase
+    {
+        public void Initialize()
+        {
+            Task.Run(async () =>
+            {
+                var status = await Permissions.CheckStatusAsync<Permissions.Camera>();
+                if (status == PermissionStatus.Denied || status == PermissionStatus.Unknown)
+                {
+                    status = await Permissions.RequestAsync<Permissions.Camera>();
+                }
+            });
+        }
+    }
+}
--- ExpertsBlog.Mobile/ViewModels/GeolocationViewModel.cs
+++ ExpertsBlog.Mobile/ViewModels/GeolocationViewModel.cs
@@ -17,17 +17,28 @@ namespace ExpertsBlog.Mobile.ViewModels
         {
             Task.Run(async () =>
             {
-                var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
-                if (status == PermissionStatus.Denied || status == PermissionStatus.Unknown)
+                Console.WriteLine("Toto");
+                Permissions.LocationWhenInUse locationWhenInUse = new Permissions.LocationWhenInUse();
+                //var requiredDeclarations = locationWhenInUse.RequiredDeclarations;
+                //var requiredInfoPlistKeys = locationWhenInUse.RequiredInfoPlistKeys;
+                //var requiredPermissions = locationWhenInUse.RequiredPermissions;
+                locationWhenInUse.EnsureDeclared();
+
+
+                //var isCapacityDeclared = Permissions.IsCapacityDeclared(Permissions.LocationWhenInUse);
+                var shouldShowRationale = Permissions.ShouldShowRationale<Permissions.LocationWhenInUse>();
+                var locationCheckStatus = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+                if (locationCheckStatus != PermissionStatus.Granted)
                 {
-                    status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+                    var locationRequestStatuts = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+                    var s = locationWhenInUse.RequestAsync();
                 }
 
                 // Geolocation
 
                 try
                 {
-                    var location = await Geolocation.GetLastKnownLocationAsync();
+                    var location = await Geolocation.GetLocationAsync();
 
                     if (location != null)
                     {
