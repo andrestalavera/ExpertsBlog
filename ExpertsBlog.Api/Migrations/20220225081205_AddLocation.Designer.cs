9c0df77efb021de43641039f0dce5e15720c8192
Andrés Talavera <andres.talavera@live.fr>
2022-02-25 10:15:19 +0100

    Add location into Addresses table

--- ExpertsBlog.Api/Migrations/20220225081205_AddLocation.Designer.cs
+++ ExpertsBlog.Api/Migrations/20220225081205_AddLocation.Designer.cs
@@ -4,6 +4,7 @@ using ExpertsBlog.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ExpertsBlog.Api.Migrations
 {
     [DbContext(typeof(ExpertsBlogDbContext))]
-    partial class ExpertsBlogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220225081205_AddLocation")]
+    partial class AddLocation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
